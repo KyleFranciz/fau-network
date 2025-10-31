7d1e0cafeb90d6eb068f5cc725930d336e3fe5cf
Kyle <sjack421@gmail.com>
2025-10-31 13:16:50 -0400

    Started initial work on the eventpage
    - added searchbar so far, adding selection and sorting Api routes later
    on.

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,15 @@
+// SearchBar.tsx
+
+// TODO: Add functionality later on
+const SearchBar = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Find events"
+      className="rounded-4xl w-[600px] mx-auto p-4 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
+};
+
+export default SearchBar;
+
--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -1,9 +1,14 @@
+import SearchBar from "../components/SearchBar";
 import FeaturedEvents from "./homeComponents/FeaturedEvents";
 
 export default function EventsPage() {
   return (
     <div className="w-full">
+      <div className="flex justify-center mt-8">
+        <SearchBar />
+      </div>
       {/* TODO: Add a searchbar here */}
+      {/* TODO: Add a selector to help with the sorting of the events */}
       <section>
         <FeaturedEvents />
       </section>
