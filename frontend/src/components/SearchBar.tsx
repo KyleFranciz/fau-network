4418c601ec0a61365beeee8541c155e6e6747ed3
Kyle <sjack421@gmail.com>
2025-11-04 19:47:25 -0500

    Edited the searchbar
    - editted the searchbar to have add in a search button (adding
    functionality later on)

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,15 +1,23 @@
-// SearchBar.tsx
+import { Search } from "lucide-react";
 
 // TODO: Add functionality later on
 const SearchBar = () => {
   return (
-    <input
-      type="text"
-      placeholder="Find events"
-      className="rounded-4xl w-[600px] mx-auto p-4 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-    />
+    <div className="relative mx-auto w-[600px]">
+      <input
+        type="text"
+        placeholder="Find events"
+        className="w-full rounded-4xl border border-gray-300 p-4 pr-14 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button
+        type="button"
+        aria-label="Search events"
+        className="absolute right-3 top-1/2 flex h-10 w-10 -translate-y-1/2 items-center justify-center rounded-full bg-black text-white transition-colors hover:bg-gray-800"
+      >
+        <Search className="h-5 w-5" />
+      </button>
+    </div>
   );
 };
 
 export default SearchBar;
-
